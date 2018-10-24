4c6eb6ac7de9f0bddedbb5c4685ac901fe83be19
farso <isfores@gmail.com>
2018-10-24 11:37:47 +0200

    new lucky word method

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,16 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
-    public function number()
+    /**
+     * @Route("/lucky/number/{max}")
+     */
+    public function number($max)
     {
-        $number = random_int(0,100);
+        $number = random_int(0, $max);
         
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
-
+    }
+    
+    /**
+     * @Route("/lucky/word/{len}")
+     */
+    public function word($len = 1)
+    {
+        $word = array_merge(range('a', 'z'), range('A', 'Z'));
+        shuffle($word);
+        
+        $word = substr(implode($word), 0, $len);
+        
+        return new Response(
+            '<html><body>Lucky word: '.$word.'</body></html>'
+        );
     }
 }
